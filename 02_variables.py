b00c3391ded816fc1491f87028f89524242fbbe5
Suhayl88 <101679044+Suhayl88@users.noreply.github.com>
2022-03-18 10:34:24 +1300

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,15 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num=7
+print(fav_num * 2)
+
+# this variable has a problem
+my_name = "Suhayl88"
+
+print(greeting)
+print(fav_num)
+print(my_name)
